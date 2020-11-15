bf4819e8465c5c5725559892f071421cc5367909
Jarrod Ng <jarrodngjg@gmail.com>
2020-11-15 18:38:08 +0800

    Update add material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
+
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => {
+  root: {
+  }
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={classes.root}>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +25,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
